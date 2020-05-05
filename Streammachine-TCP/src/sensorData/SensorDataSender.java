f0d993a469043e3761c04f0eed6131f2f5e4c422
Ostervogel <34425590+Ostervogel@users.noreply.github.com>
2020-05-05 22:55:49 +0200

    Add files via upload

---
+++ Streammachine-TCP/src/sensorData/SensorDataSender.java
@@ -0,0 +1,17 @@
+package sensorData;
+
+import transmission.DataConnection;
+
+import java.io.IOException;
+
+public class SensorDataSender {
+    private final DataConnection connection;
+
+    public SensorDataSender(DataConnection connection) {
+        this.connection = connection;
+    }
+
+    public void sendData(String name, long time, float[] values) throws IOException {
+        // TODO
+    }
+}
---
+++ Streammachine-TCP/src/test/transmission/TransmissionTests.java
@@ -0,0 +1,35 @@
+package test.transmission;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import transmission.DataConnection;
+import transmission.DataConnector;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class TransmissionTests {
+    private static final int PORTNUMBER = 9876;
+    private static final int TEST_INT = 42;
+
+    @Test
+    public void gutConnectionTest1() throws IOException, InterruptedException {
+        // open server side
+        DataConnection serverSide = new DataConnector(PORTNUMBER);
+
+        // open client side
+        DataConnection clientSide = new DataConnector("localhost", PORTNUMBER);
+
+        DataOutputStream dataOutputStream = clientSide.getDataOutputStream();
+        dataOutputStream.writeInt(TEST_INT);
+
+        DataInputStream dataInputStream = serverSide.getDataInputStream();
+        int readValue = dataInputStream.readInt();
+
+        Assert.assertEquals(TEST_INT, readValue);
+    }
+
+
+}
